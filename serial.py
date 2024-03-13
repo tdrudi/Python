2cc829332b5ded2d20e0408682e7a6de9b39782e
tdrudi <145171710+tdrudi@users.noreply.github.com>
2024-03-13 11:43:02 -0400

    Add OOP

---
+++ serial.py
@@ -0,0 +1,30 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+    def __init__(self, start = 0):
+        self.start = self.next = start
+    def generate(self):
+        self.next += 1
+        return self.next -1
+    def reset(self):
+        self.next = self.start
+
+
